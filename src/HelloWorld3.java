e7670d26efd8d45332ff980844edbed17377ad15
LipinskayaAlena <lipinskaya_alena@mail.ru>
2018-02-23 09:23:38 +0300

    edit

--- src/HelloWorld3.java
+++ src/HelloWorld3.java
@@ -1,7 +1,5 @@
-
 public class HelloWorld3 {
-	public static void main(String[] args)
-	  {
-	   System.out.println("Hello World!");
-	  }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
